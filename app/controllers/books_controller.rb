875f52f6fbdb63deb4646323613d933aebfbd3ce
cohki0305 <cohki0305@gmail.com>
2018-08-27 21:53:09 +0800

    api

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,35 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
-    # render json: books
   end
 
   def show
     @book = Book.find(params[:id])
+  end
+
+  def create
+    @book = Book.create!(book_params)
+    render 'show'
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update!(book_params)
+    render 'show'
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy!
+    render 'show'
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(
+      :title,
+      :author,
+      :isbn,
+    )
   end
 end
